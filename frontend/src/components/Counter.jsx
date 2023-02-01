5bb0f07433d2c184d7d734baaeb78bf5324fb80e
c.roy3 <c.roy3728@gmail.com>
2023-02-01 12:07:50 +0100

    add files and modifications

--- frontend/src/components/Counter.jsx
+++ frontend/src/components/Counter.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
+  // const url = process.env.VITE_BACKEND_URL;
 
   return (
     <p>
+      {/* url */}
       <button
         type="button"
         onClick={() => setCount((oldCount) => oldCount + 1)}
