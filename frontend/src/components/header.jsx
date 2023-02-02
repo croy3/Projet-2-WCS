97eb0e4fc9e5b843ca3323622c3588359d4228c9
Alix Philibert <alix2035@gmail.com>
2023-02-02 16:14:45 +0100

    basic use header is done

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function MainHeader() {
+  return (
+    <div className="Header">
+      <img src="http://placekitten.com/200/300" alt="temp" className="Logo" />
+      <h1 className="title">Titre de la page</h1>
+    </div>
+  );
+}
+export default MainHeader;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Counter from "../components/Counter";
 import logo from "../assets/logo.svg";
+import MainHeader from "../components/header";
 
 export default function Home() {
   return (
     <header className="App-header">
+      <MainHeader />
       <img src={logo} className="App-logo" alt="logo" />
       <p>Hello Vite + React !</p>
 
