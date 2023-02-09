ded9d1c8bb91e6e284dce7a7c142b45780f1a13a
Alix Philibert <alix2035@gmail.com>
2023-02-09 14:07:41 +0100

    changed class name and media query added

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,9 +3,13 @@ import "./header.css";
 
 function MainHeader() {
   return (
-    <div className="Header">
-      <img src="http://placekitten.com/200/300" alt="temp" className="Logo" />
-      <h1 className="title">Titre de la page</h1>
+    <div className="header-main">
+      <img
+        src="http://placekitten.com/200/300"
+        alt="temp"
+        className="header-logo"
+      />
+      <h1 className="header-title">Titre de la page</h1>
     </div>
   );
 }
